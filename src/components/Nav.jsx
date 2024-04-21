ea70c61728e295cf5c84c93d139c719ffc6d4543
kazeem lawal <quazeemastic@gmail.com>
2024-04-21 18:18:31 +0100

    updated Nav section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,10 @@
 import {headerLogo} from '../assets/images';
+import {Navlinks} from '../constants
 
 const Nav = () => {
     return(
         <header clasName="padding-x py-8 absolute z-10 w-full">
-        <nav class name = "flex justify-between items-center max-conatiner">
+        <nav class name = "flex justify-between items-center max-container">
         <a href ="/">
         <img
         src ={headerLogo}
@@ -13,7 +14,21 @@ const Nav = () => {
         />
         </a>
         <ul className = "flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-        </ul>
+       {navlinks.map((item) => (
+	<li key = item.label>
+	<a href = {item.href} className = "font-montsorrat leading-normal text-lg text-slate-gray">
+	{item.label}
+	</a>
+	</li>
+
+		))}
+ </ul>
+	<div className = "hidden max-lg:block">
+	<img src ={hamburger}
+	alt = "hamburger"
+	width = {25}
+	height = {25}/>
+	</div>
         </nav>
         </header>
         )
