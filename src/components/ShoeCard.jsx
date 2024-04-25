38720bcc324ec9e1a22fa6df5520c47ee5b39582
Kazeem Lawal <quazeemastic@gmail.com>
2024-04-26 00:22:35 +0100

    fixed useState bug

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -6,7 +6,7 @@ const ShoeCard = ({imgURL,changeBigShoeImage,BigShoeImg}) => {
         }
     }
 	return(
-		<div className={`border-2 rounded-xl ${bigShoeImg === imgURL ? 'border-coral-red':'border-transparent'}cursor-pointer max-sm:flex-1`}
+		<div className={`border-2 rounded-xl ${BigShoeImg === imgURL ? 'border-coral-red':'border-transparent'}cursor-pointer max-sm:flex-1`}
 		onClick ={handleClick}>
 	<div className="flex justify-center items-center bg-card bg-center bg-cover sm:h-49 rounded-xl max-sm: p-4">
 	<img src={imgURL.thumbnail} 
--- src/sections/Heroes.jsx
+++ src/sections/Heroes.jsx
@@ -17,7 +17,7 @@ const Heroes = () => {
         <h1 className="mt-10 font-palanquin font-bold text-8xl max-sm:text-[72px] max-sm:leading-[82px]">
         <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10" >The New Arrival</span>
         <br/>
-        <span clasName="text-coral-red inline-block mt-3">Nike</span> Shoes
+        <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
         <Button label="shop now"
@@ -35,7 +35,7 @@ const Heroes = () => {
 	<div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
 	    <img 
 
-	    src = {bigShoe1}
+	    src = {bigShoeImg}
 	    alt="shoe collection"
 	    width={610}
 	    height={500}
